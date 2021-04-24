10aeccba8e4066334fa2ba3ec0915fca323290ab
CJJ <cassio.jjesus@gmail.com>
2021-04-24 09:00:30 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>NIDODesk</title>
+      </Head>
+
+      <main>
+        <h1>NIDODesk</h1>
+      </main>
+    </div>
+  );
+}
